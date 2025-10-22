f2c85db839c695b86ac0dd9f6523ced67729f2d6
Arash Edalatpanah <aea53@sfu.ca>
2025-10-22 15:03:58 -0700

    Updatde the filter script

--- filter.py
+++ filter.py
@@ -1,6 +1,19 @@
+import re
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+def singular_form(word):
+    word = word.lower().strip()
+    if word.endswith("'s"):
+        return word[:-2]
+    return word
+
 
 input_file = "cmu_dict_cleaned.csv"
 output_file = "cmu_dict_cleaned_filtered.csv"
+
+seen = set()
 
 with open(input_file, "r", encoding="utf-8") as infile, open(output_file, "w", encoding="utf-8") as outfile:
     for line in infile:
@@ -8,9 +21,20 @@ with open(input_file, "r", encoding="utf-8") as infile, open(output_file, "w", e
         if not line:
             continue
 
-        first_word = line.split(',')[0]
+        word = line.split(',')[0]
+        word = singular_form(word)
 
-        if len(first_word) < 6:
+
+        base = lemmatizer.lemmatize(word, pos='n')
+        base = singular_form(base)
+
+
+        if base in seen:
+            continue
+
+        seen.add(word)
+
+        if len(word) > 3 and word.isalpha():
             outfile.write(line + "\n")
 
 print("Filtering complete. Output written to", output_file)
